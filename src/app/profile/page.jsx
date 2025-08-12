930505f4b414cc6ff206e8df0b47ce3ad041ac93
SharifzodaUmar <SharifzodaUmar>
2025-08-12 18:07:28 +0000

    First section of profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,62 @@
+'use client'
+import Image from "next/image";
+import p from '../../assets/img/pages/profile/profile/p.png'
+import { useGetMyProfileQuery } from "@/store/pages/profile/ProfileApi";
+
 export default function Profile() {
+
+  const { data, isLoading, isError, refetch } = useGetMyProfileQuery();
+   const profile = data?.data;
+
+
+ if (isLoading) return <p>Загрузка продуктов...</p>;
+  if (isError) return <p>Ошибка при загрузке данных</p>;
 	return (
 		<>
-			<h1>Page profile</h1>
+			<div className="sm:max-w-[640px] bg-blue-600 ml-[100px] ">
+
+
+
+<section className="w-full flex items-center justify-between h-[160px] bg-amber-600">
+<Image
+  alt="Profile photo"
+  width={160}
+  height={160}
+  src={profile.image === "" ? `${p.src}` : profile.image}
+/>
+
+
+<article className="sm:w-[456px] flex flex-col items-start justify-between sm:h-[142px] bg-cyan-600">
+<div className="w-full flex items-start justify-between">
+<p className="text-[20px] font-[700]">{profile.userName}</p>
+<button className="bg-[#F3F4F6] w-[105px] h-[40px] rounded-xl flex items-center justify-center">Edit profile</button>
+<button className="bg-[#F3F4F6] w-[105px] h-[40px] rounded-xl flex items-center justify-center">View archive</button>
+<p>----</p>
+</div>
+
+
+
+<div className="flex w-full items-start gap-7">
+<p className="text-[14px] font-[600]"> {profile.postCount} <span className="text-[#64748B] font-[400]">posts</span></p>
+<p className="text-[14px] font-[600]"> {profile.subscribersCount} <span className="text-[#64748B] font-[400]">followers</span></p>
+<p className="text-[14px] font-[600]"> {profile.subscriptionsCount} <span className="text-[#64748B] font-[400]">following</span></p>
+</div>
+
+<p className="text-[20px] font-[700]">{profile.firstName}</p>
+
+</article>
+
+</section>
+
+<section>
+		
+</section>
+
+
+
+
+
+			</div>
 		</>
 	)
 }
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { storeApi } from "@/store/pages/chat/pages/storeApi";
 import { authApi } from "./pages/auth/registration/registerApi";
 import { muslimApi } from "./pages/home/muslimApi";
-
+import { profileApi } from "./pages/profile/ProfileApi";
 export const store = configureStore({
   reducer: {
     [storeApi.reducerPath]: storeApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [muslimApi.reducerPath]: muslimApi.reducer,
+    [profileApi.reducerPath]: profileApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
@@ -15,4 +16,5 @@ export const store = configureStore({
       .concat(storeApi.middleware)
       .concat(authApi.middleware)
       .concat(muslimApi.middleware)
+      .concat(profileApi.middleware)
 });
