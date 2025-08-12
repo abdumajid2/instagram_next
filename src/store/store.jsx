0e33e3f124c9dc04be9518c8f6bbac4460ba3596
abdumajid2 <abdumajidt89@gmail.com>
2025-08-12 16:44:22 +0500

    duvozdah

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -4,31 +4,28 @@ import { authApi } from "./pages/auth/registration/registerApi";
 
 import { settingApi } from './pages/setting/settingApi'
 
-
-export const store = configureStore({
-  reducer: {
-    [storeApi .reducerPath]: storeApi .reducer,
-    [authApi.reducerPath]: authApi.reducer,
-    [settingApi.reducer]: settingApi.reducer
-
 import { muslimApi } from "./pages/home/muslimApi";
+
+
+
 
 export const store = configureStore({
   reducer: {
     [storeApi.reducerPath]: storeApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [muslimApi.reducerPath]: muslimApi.reducer,
+    [settingApi.reducerPath]: settingApi.reducer,
 
   },
 
   middleware: (getDefaultMiddleware) =>
 
-    getDefaultMiddleware().concat(storeApi .middleware)
-    .concat(authApi.middleware).concat(settingApi.middleware)
+    
 
     getDefaultMiddleware()
       .concat(storeApi.middleware)
       .concat(authApi.middleware)
       .concat(muslimApi.middleware)
+      .concat(settingApi.middleware)
 
 });
