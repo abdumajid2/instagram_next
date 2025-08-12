77e9cbda66740c5b1bea0b85695cddc5facf69ae
Muslihiddin01 <mahmadalievmuslihiddin@gmail.com>
2025-08-12 16:28:26 +0500

    muslimCommit

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { storeApi  } from "@/store/pages/chat/pages/storeApi";
+import { storeApi } from "@/store/pages/chat/pages/storeApi";
 import { authApi } from "./pages/auth/registration/registerApi";
-
+import { muslimApi } from "./pages/home/muslimApi";
 
 export const store = configureStore({
   reducer: {
-    [storeApi .reducerPath]: storeApi .reducer,
-    [authApi.reducerPath]: authApi.reducer
+    [storeApi.reducerPath]: storeApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [muslimApi.reducerPath]: muslimApi.reducer,
   },
-  
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(storeApi .middleware)
-    .concat(authApi.middleware)
-});
 
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(storeApi.middleware)
+      .concat(authApi.middleware)
+      .concat(muslimApi.middleware)
+});
