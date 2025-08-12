ac4525e2c731a832593d0ee409d197ae0bf4ad70
abdumajid2 <abdumajidt89@gmail.com>
2025-08-12 14:22:44 +0500

    haft

--- src/app/(auth)/registration/page.jsx
+++ src/app/(auth)/registration/page.jsx
@@ -1,6 +1,6 @@
 export default function Registration(){
 	return <>
 		<h1>Page registration</h1>
-		
+		{/* https://www.figma.com/design/zn8hEOYgm1CILVObnDCd1q/Instagram?node-id=8-3066&t=DLj5O2ryiwh57rkm-0 */}
 	</>
 }
