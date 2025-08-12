12c8ee4eaa4d17039ab1e5164de3239f1ee3dd70
Ahliddinpr1 <holovalijon615@gmail.com>
2025-08-12 17:42:48 +0500

    Fixed

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,46 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { storeApi } from "@/store/pages/chat/pages/storeApi";
 import { authApi } from "./pages/auth/registration/registerApi";
-<<<<<<< HEAD
-import { ReelsApi } from "./pages/reels/ReelsApi";
-
-export const store = configureStore({
-  reducer: {
-    [storeApi .reducerPath]: storeApi .reducer,
-    [authApi.reducerPath]: authApi.reducer,
-    [ReelsApi.reducerPath]: ReelsApi.reducer,
-  },
-  
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(storeApi .middleware)
-    .concat(authApi.middleware).concat(ReelsApi.middleware)
-});
-=======
-
-import { settingApi } from './pages/setting/settingApi'
-
 import { muslimApi } from "./pages/home/muslimApi";
-
-
-
+import { ReelsApi } from "./pages/reels/ReelsApi";
 
 export const store = configureStore({
   reducer: {
     [storeApi.reducerPath]: storeApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [muslimApi.reducerPath]: muslimApi.reducer,
-    [settingApi.reducerPath]: settingApi.reducer,
+    [ReelsApi.reducerPath]: ReelsApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-
-    
-
     getDefaultMiddleware()
       .concat(storeApi.middleware)
       .concat(authApi.middleware)
       .concat(muslimApi.middleware)
-      .concat(settingApi.middleware)
-
+      .concat(ReelsApi.middleware)
 });
->>>>>>> 2ddd73064b2e268dbd6ca1fc6b8dc4e2e2f20a77
