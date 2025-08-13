bcba190b6ffb5f95b71574cf9a4ce306ef1e8a44
Sayfov Aliakbar <sayfovsliakbar.2009@gmail.com>
2025-08-13 16:29:59 +0500

    Ending Search Page

--- src/store/pages/setting/settingApi.jsx
+++ src/store/pages/setting/settingApi.jsx
@@ -1,15 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
-
-export const settingApi  = createApi({
-  reducerPath: "storeApi ",
+export const settingApi = createApi({
+  reducerPath: "storeApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://37.27.29.18:8003/", 
+    baseUrl: "http://37.27.29.18:8003",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiJjOGZmNDU3OC0wYzM5LTQxOTgtYmVjYy1jZjU3YTIzYzA4MzMiLCJuYW1lIjoidmp4aW5nIiwiZW1haWwiOiJBbGlha2JhckBnbWFpbC5jb20iLCJzdWIiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJVc2VyIiwiZXhwIjoxNzU1MTUwMDcwLCJpc3MiOiJpbnN0YWdyYW0tZ3JvdXAiLCJhdWQiOiJpbnN0YWdyYW0tYXBpIn0.G0DVjYa0X-KVvWFHC4rxt0F49wBr-4_UwLfk6MaLt7A" // вставь сюда токен
+      );
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
-    
+    getUsersForSearch: builder.query({
+      query: ({ pageNumber = 1, pageSize = 9999 } = {}) => ({
+        url: `/User/get-users`,
+        method: "GET",
+        params: {
+          PageNumber: pageNumber,
+          PageSize: pageSize,
+        },
+      }),
+    }),
   }),
 });
 
-export const {  } = settingApi ;
+export const { useGetUsersForSearchQuery } = settingApi;
