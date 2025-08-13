33235abb96a1160c4edd3cd36c92528b6cd9cd48
Ahliddinpr1 <holovalijon615@gmail.com>
2025-08-13 19:59:58 +0500

    fixed

---
+++ src/components/pages/reels/ReelsSlider.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './ReelsSlider.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+export default function ReelsSlider( { reels } ) {
+  return (
+    <>
+      <Swiper
+        direction={'vertical'}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {reels.map((e, i) => {
+            return <SwiperSlide>Slide 1</SwiperSlide>
+        })}
+      </Swiper>
+    </>
+  );
+}
